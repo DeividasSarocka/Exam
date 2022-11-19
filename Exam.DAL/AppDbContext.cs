d0c936c58bb7ea5f7c5a3b7127fd3045007ba73f
DeividasSar <deividas.sarocka@gmail.com>
2022-11-19 16:24:10 +0200

    Addded migrations

---
+++ Exam.DAL/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Exam.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.DAL
+{
+    public class AppDbContext : DbContext
+    {
+
+            public DbSet<UserAccount> UserAccounts { get; set; }  
+            public DbSet<PersonalInfo> PersonalInfos { get; set; }
+            public DbSet<ResidentialInfo> ResidentialInfos { get; set; }
+            public AppDbContext(DbContextOptions options) : base(options)  
+            {
+            }
+        
+    }
+}
---
+++ Exam.DAL/Migrations/20221119142153_initialCreate.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Exam.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ResidentialInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StreetName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HouseNumber = table.Column<int>(type: "int", nullable: false),
+                    ApartmentNumber = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResidentialInfos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonalInfos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonalCode = table.Column<int>(type: "int", nullable: false),
+                    PhoneNumber = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Image = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    ResidentialInfoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonalInfos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PersonalInfos_ResidentialInfos_ResidentialInfoId",
+                        column: x => x.ResidentialInfoId,
+                        principalTable: "ResidentialInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserAccounts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    Role = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PersonalInfoId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserAccounts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserAccounts_PersonalInfos_PersonalInfoId",
+                        column: x => x.PersonalInfoId,
+                        principalTable: "PersonalInfos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonalInfos_ResidentialInfoId",
+                table: "PersonalInfos",
+                column: "ResidentialInfoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserAccounts_PersonalInfoId",
+                table: "UserAccounts",
+                column: "PersonalInfoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserAccounts");
+
+            migrationBuilder.DropTable(
+                name: "PersonalInfos");
+
+            migrationBuilder.DropTable(
+                name: "ResidentialInfos");
+        }
+    }
+}
